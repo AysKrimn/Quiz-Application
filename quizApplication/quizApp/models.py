5b8ca9f335b03cd17c28fc0a5af5d83661e465b4
Oğuz Doğan <129693344+OguzDogan1992@users.noreply.github.com>
2023-05-13 15:53:53 +0300

    Add files via upload

--- quizApplication/quizApp/models.py
+++ quizApplication/quizApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    detail = models.TextField()
+    def __str__(self):
+        return self.title
+    
+class Quizes(models.Model):
+    title = models.CharField(max_length=35)
+    detail = models.TextField()
+    date_joined = models.DateTimeField(default=timezone.now)
+    banner = models.FileField(upload_to='static/img', null=True)
+    timer = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return self.title
