3fc7d61b131f3b2720fd8ca94863974401f0db9e
Oğuz Doğan <129693344+OguzDogan1992@users.noreply.github.com>
2023-05-15 15:17:24 +0300

    Add files via upload

--- quizApplication/quizApp/admin.py
+++ quizApplication/quizApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class QuizesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category','detail' ,'date_joined', 'timer')
+    list_filter = ('title', 'category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'detail')
+    list_filter = ('title', 'detail')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Quizes, QuizesAdmin)
