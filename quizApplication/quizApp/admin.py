50315dbf08635e96ac8816f4cfdfc8156c5f1fb9
Oğuz Doğan <129693344+OguzDogan1992@users.noreply.github.com>
2023-05-15 18:55:47 +0300

    Add files via upload

--- quizApplication/quizApp/admin.py
+++ quizApplication/quizApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+
 class QuizesAdmin(admin.ModelAdmin):
     list_display = ('title', 'category','detail' ,'date_joined', 'timer')
     list_filter = ('title', 'category')
@@ -9,5 +10,10 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'detail')
     list_filter = ('title', 'detail')
 
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'email')
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Quizes, QuizesAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
--- quizApplication/quizApp/models.py
+++ quizApplication/quizApp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
+
+class CustomUser(AbstractUser):
+    profile_picture = models.FileField(upload_to='quizApp/static/profilePicture', null=True)
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
@@ -12,7 +16,7 @@ class Quizes(models.Model):
     title = models.CharField(max_length=35)
     detail = models.TextField()
     date_joined = models.DateTimeField(default=timezone.now)
-    banner = models.FileField(upload_to='static/img', null=True)
+    banner = models.FileField(upload_to='quizApp/static/quizBanner', null=True)
     timer = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     def __str__(self):
