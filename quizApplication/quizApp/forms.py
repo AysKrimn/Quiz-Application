b308c3f71b2c57ec86e31c1e32099aefa3b58974
_omerock <83617943+AysKrimn@users.noreply.github.com>
2023-05-20 16:14:31 +0300

    Delete quizApplication directory

--- quizApplication/quizApp/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'first_name','last_name', 'password1', 'password2', 'profile_picture']
--- quizApplication/quizApp/static/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-
-      $(document).ready(function(){
-        $('.menu-toggle').click(function(){
-          $('.menu-toggle').toggleClass('active')
-          $('.menu').toggleClass('active')
-        });
-      });
-
